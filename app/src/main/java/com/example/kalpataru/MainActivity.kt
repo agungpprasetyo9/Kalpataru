279119e86607d7762c914c15c365e98b07c055da
agungpprasetyo9 <aprasetyo6911@gmail.com>
2023-11-19 13:06:18 +0700

    half dashboard

--- app/src/main/java/com/example/kalpataru/MainActivity.kt
+++ app/src/main/java/com/example/kalpataru/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.kalpataru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Assuming you have a Button in your layout with the ID "button"
+        val button: Button = findViewById(R.id.button)
+
+        // Set an OnClickListener for the button
+        button.setOnClickListener {
+            // Create an Intent to start SecondActivity
+            val intent = Intent(this, Dashboard::class.java)
+
+            // Optionally, you can pass data to the second activity using extras
+            intent.putExtra("key", "Hello from MainActivity!")
+
+            // Start the second activity
+            startActivity(intent)
+        }
     }
 }
